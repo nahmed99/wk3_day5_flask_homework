e18661ed9b04f31c4b480725612fe024eb7caed5
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-20 21:22:44 +0000

    Created 9 games tests; 3 x draws, 2 x rock vs scissors (True and False), 2 x scissors vs paper (True and False), 2 x paper vs rock (True and False)

--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -6,22 +6,81 @@ from app.models.player import *
 class GameTest(unittest.TestCase):
 
     def setUp(self):
-        self.player_1a = Player("Dick Turpin", "rock")
-        self.player_2a = Player("Robin Hood", "scissors")
-        self.game_1 = Game(self.player_1a, self.player_2a)
+        self.player_1_rock = Player("Dick Turpin", "rock")
+        self.player_1_scissors = Player("Dick Turpin", "scissors")
+        self.player_1_paper = Player("Dick Turpin", "paper")
+        self.player_2_rock = Player("Robin Hood", "rock")
+        self.player_2_scissors = Player("Robin Hood", "scissors")
+        self.player_2_paper = Player("Robin Hood", "paper")
 
-        self.player_1b = Player("Dick Turpin", "scissors")
-        self.player_2b = Player("Robin Hood", "rock")
-        self.game_2 = Game(self.player_1b, self.player_2b)
+        # rock draws with rock
+        self.game_rr = Game(self.player_1_rock, self.player_2_rock)
+
+        # scissors draws with rock
+        self.game_ss = Game(self.player_1_scissors, self.player_2_scissors)
+
+        # paper draws with paper
+        self.game_pp = Game(self.player_1_paper, self.player_2_paper)
+
+        # rock beats scissors
+        self.game_rs = Game(self.player_1_rock, self.player_2_scissors)
+        self.game_sr = Game(self.player_1_scissors, self.player_2_rock)
+
+        # scissors beats paper
+        self.game_sp = Game(self.player_1_scissors, self.player_2_paper)
+        self.game_ps = Game(self.player_1_paper, self.player_2_scissors)
+
+        # paper beats rock
+        self.game_pr = Game(self.player_1_paper, self.player_2_rock)
+        self.game_rp = Game(self.player_1_rock, self.player_2_paper)
 
 
-    def test_player_1_wins(self):
+    # === Test draws ===
+
+    def test_payer_1_draws_r(self):
+        # both players have rock
+        self.assertIsNone(self.game_rr.check_winner())
+
+    def test_payer_1_draws_s(self):
+        # both players have scissors
+        self.assertIsNone(self.game_ss.check_winner())
+
+    def test_payer_1_draws_p(self):
+        # both players have paper
+        self.assertIsNone(self.game_pp.check_winner())
+
+
+    # === Test rock beats scissors ===
+
+    def test_player_1_wins_rs(self):
         # test rock beats scissors
-        self.game_1.check_winner()
-        self.assertEqual(True, self.game_1.check_winner())
+        self.assertEqual(True, self.game_rs.check_winner())
+    
+    def test_player_1_loses_sr(self):
+        # test scissors loses to rock
+        self.assertEqual(False, self.game_sr.check_winner())
+
+
+    # === Test scissors beats paper ===
+
+    def test_player_1_wins_sp(self):
+        # test scissors beats paper
+        self.assertEqual(True, self.game_sp.check_winner())
+
+    def test_player_1_loses_ps(self):
+        # test paper loses to scissors
+        self.assertEqual(False, self.game_ps.check_winner())
+
+
+    # === Test paper beats rock ===
+
+    def test_player_1_wins_pr(self):
+        # test paper beats rock
+        self.assertEqual(True, self.game_pr.check_winner())
     
 
-    def test_player_1_loses(self):
-        # test rock beats scissors
-        self.game_2.check_winner()
-        self.assertEqual(False, self.game_2.check_winner())
+    def test_player_1_loses_rp(self):
+        # test rock loses to paper
+        self.assertEqual(False, self.game_rp.check_winner())
+
+
