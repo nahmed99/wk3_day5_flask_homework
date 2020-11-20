59e226d6679784c581bf020a9a1c859c504445ef
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-20 22:22:52 +0000

    removed home/root "/", by redirecting it to "/welcome" page

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 # from flask import render_template, request, redirect - don't need request and redirect for the simple 'GET' in the initial, MVP part of the application
-from flask import render_template
+from flask import render_template, redirect
 from app import app
 from app.models.game import Game
 from app.models.player import Player
@@ -10,10 +10,22 @@ from app.models.player import Player
 # The controller sits and listens, waiting for requests that will come in
 
 # This defaults to a GET request
+# @app.route('/') # This listens for 'requests' to the home/root
+# def index():
+#     # This and other below are normal python functions...
+#     return render_template('index.html', title="Home")
+
+
 @app.route('/') # This listens for 'requests' to the home/root
 def index():
+    # This will redirect the user to the welcome page...
+    return redirect("/welcome")
+
+
+@app.route('/welcome') # This listens for 'requests' to the home/root
+def welcome():
     # This and other below are normal python functions...
-    return render_template('index.html', title="Home")
+    return render_template('welcome.html', title="Welcome")
 
 
 @app.route('/<hand1>/<hand2>')
@@ -32,4 +44,5 @@ def play_game(hand1, hand2):
         result += "The winner is player 2"
     
     # return f"Player 1 had {hand1} and player 2 had {hand2}."
-    return result
+    # return result
+    return render_template('result.html', title="Result", result=result)
